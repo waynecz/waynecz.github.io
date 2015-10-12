1c21dbdf86536dd467fcc10498cb6380aac726f4
Wayne <chengzhou.weng@fraudmetrix.cn>
2015-10-12 10:51:10 +0800

    Site updated: 2015-10-12 10:51:09

--- js/script.js
+++ js/script.js
@@ -1,28 +1,59 @@
-(function($){
-
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
-
-      var alt = this.alt;
-
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+(function($) {
+    // Caption
+    $('.article-entry').each(function(i) {
+        $(this).find('img').each(function() {
+            if ($(this).parent().hasClass('fancybox')) return;
+            var alt = this.alt;
+            if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+            $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+        });
+        $(this).find('.fancybox').each(function() {
+            $(this).attr('rel', 'article' + i);
+        });
     });
-
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
+    if ($.fancybox) {
+        $('.fancybox').fancybox();
+    }
+    // Mobile nav
+    $('#main-nav-toggle').click(function() {
+        $('#header').toggleClass('mobile-on');
     });
-  });
-
-  if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
-
-  // Mobile nav
-  $('#main-nav-toggle').click(function () {
-    $('#header').toggleClass('mobile-on');
-  });
 })(jQuery);
+$(function() {
+    var inIndex = function() {
+        var url = window.location.href,
+            winHeight = $(window).height(),
+            lock = 0;
+        if (url == "http://localhost:4000/") {
+            lock = 1;
+        };
+        return lock;
+    };
+    if (inIndex()) {
+        $('.welcome').addClass('index');
+        $('#container').addClass('rollDown').delay(900);
+        $('#word').find('span').addClass('zoomIn')
+    } else {
+        $('#main, #footer').addClass('topIn').css({
+            'opacity': '1',
+            'transform': 'translate3d(0,0,0)'
+        })
+    }
+    $(window).scroll(function() {
+        var words = $('#word').find('span'),
+            top = $(window).scrollTop();
+        $.each(words, function(i, e) {
+            var ratio = $(e).attr('data-ratio');
+            $(e).css('transform', 'translate3d(0,' + top * ratio + 'px,0)');
+        })
+        $('.wordWrap').css('transform', 'translate3d(0,' + top * -0.3 + 'px,0)');
+        if (inIndex()) {
+            if (top >= 290) {
+                $('#main, #footer').addClass('topIn').css({
+                    'opacity': '1',
+                    'transform': 'translate3d(0,0,0)'
+                })
+            };
+        }
+    })
+})
