5ee5c81805409a3b4f88ef2564076c0ce3274d66
Wayne <chengzhou.weng@fraudmetrix.cn>
2015-12-25 13:36:40 +0800

    Site updated: 2015-12-25 13:36:39

--- js/script.js
+++ js/script.js
@@ -1,31 +1,57 @@
 var inIndex = function() {
-        var url = window.location.href,
-            winHeight = $(window).height(),
-            lock = 0;
-        if (url == "http://waynecz.github.io/" || url == "http://localhost:4000/") {
-            lock = 1;
-        };
-        return lock;
+    var url = window.location.href,
+        winHeight = $(window).height(),
+        lock = false;
+    if (url == "http://waynecz.github.io/" || url == "http://localhost:4000/") {
+        lock = true;
     };
-$(window).load(function() {
-    // Animate loader off screen
-    $(".load-wrap").fadeOut("slow").promise().done(function(){
-        setTimeout(function(){
-            if (inIndex()) {
-                $('.welcome').addClass('index');
-                $('#container').addClass('rollDown').delay(900);
-                $('#word').find('span').addClass('zoomIn');
-            } else {
-                $('#main, #footer').addClass('topIn').css({
-                    'opacity': '1',
-                    'transform': 'translate3d(0,0,0)'
-                })
-            }
-        }, 1000);
+    return lock;
+};
+
+var t_img; // 定时器
+var isLoad = true; // 控制变量
+
+// 判断图片加载状况，加载完成后回调
+function bannerAnimation() {
+    $(".load-wrap").fadeOut(500).promise().done(function() {
+            $('.welcome').addClass('index');
+            $('#container').addClass('rollDown');
+            $('#word').find('span').addClass('zoomIn');
+        
     });
-});
+}
+
+// 判断图片加载的函数
+function isImgLoad(callback) {
+    if ($('#shadow').height === 0) {
+        // 找到为0就将isLoad设为false
+        isLoad = false;
+        return false;
+    }
+    // 为true，没有发现为0的。加载完毕
+    if (isLoad) {
+        clearTimeout(t_img); // 清除定时器
+        // 回调函数
+        callback();
+        // 为false，因为找到了没有加载完成的图，将调用定时器递归
+    } else {
+        isLoad = true;
+        t_img = setTimeout(function() {
+            isImgLoad(callback); // 递归扫描
+        }, 500); // 我这里设置的是500毫秒就扫描一次，可以自己调整
+    }
+}
 (function($) {
-    // Caption
+    if (inIndex()) {
+        isImgLoad(bannerAnimation);    
+    } else {
+        $(".load-wrap").fadeOut(300);
+        $('#main, #footer').css({
+            'opacity': '1',
+            'transform': 'translate3d(0,0,0)'
+        })
+    }
+    
     $('.article-entry').each(function(i) {
         $(this).find('img').each(function() {
             if ($(this).parent().hasClass('fancybox')) return;
@@ -47,28 +73,20 @@ $(window).load(function() {
 })(jQuery);
 
 $(function() {
-    
+
     $(window).scroll(function() {
         var words = $('#word').find('span'),
             top = $(window).scrollTop();
         $.each(words, function(i, e) {
             var ratio = $(e).attr('data-ratio');
             $(e).css('transform', 'translate3d(0,' + top * ratio + 'px,0)');
+            
         })
         $('.wordWrap').css('transform', 'translate3d(0,' + top * -0.3 + 'px,0)');
         if (inIndex()) {
             if (top >= 290) {
-                $('#main, #footer').addClass('topIn').css({
-                    'opacity': '1',
-                    'transform': 'translate3d(0,0,0)'
-                })
+                $('#main, #footer').addClass('topIn');
             };
         }
     })
 })
-
-
-
-
-
-
